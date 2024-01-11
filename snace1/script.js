cb3ee17ff22270674fd61d83cd991282d5a03f1b
Your Name <you@example.com>
2024-01-11 19:53:56 +0300

    1

--- snace1/script.js
+++ snace1/script.js
@@ -15,26 +15,35 @@ let foodCoodr = {
     x: getFoodCord(12),
     y: getFoodCord(10)
 }
+
 let snakeCord = {
     x: 5,
     y: 5
 }
 
+
+let arr = [snakeCord]
+
 function game(){
     ctx.drawImage(ground, 0, 0);
     ctx.drawImage(food, foodCoodr.x*box, foodCoodr.y*box);
 
-    ctx.fillStyle = 'green'
-    ctx.fillRect(snakeCord.x*box, snakeCord.y*box, box, box)
+    for (let i = 0; i < arr.length; i++) {
+        ctx.fillStyle = 'green'
+        ctx.fillRect(arr[i].x*box, arr[i].y*box, box, box)
+    }
+    
+    let x = arr[0].x;
+    let y = arr[0].y;
 
-    if (foodCoodr.x === snakeCord.x && foodCoodr.y === snakeCord.y ){
+    if (foodCoodr.x === x && foodCoodr.y === y ){
         score++;
         foodCoodr.x = getFoodCord(12);
         foodCoodr.y = getFoodCord(10);
+    }else{
+        arr.pop()
     }
-    if (score == 1){
-        clearInterval(gameInterval)
-    }
+
 
     ctx.fillStyle = 'white'
     ctx.font = "50px Arial";
@@ -42,21 +51,33 @@ function game(){
 
     if (napravlrenie == 37){
         // left
-        snakeCord.x--;
+        x--;
     }else if (napravlrenie == 39){
         //right
-        snakeCord.x++;
+        x++;
     }else if(napravlrenie == 38){
         // up
-        snakeCord.y--;
+        y--;
     }
     else if(napravlrenie == 40){
         // down
-        snakeCord.y++;
+        y++;
     }
+    if (napravlrenie == 27){
+        clearInterval(gameInterval)
+    }
+
+    let newHead = {
+        x: x,
+        y: y
+    }
+
+    arr.unshift(newHead);
+    console.log(x, y );
+    console.log(arr);
 }
 
-let gameInterval = setInterval(game, 300)
+let gameInterval = setInterval(game, 1000)
 
 function getFoodCord(num) {
    return Math.floor(Math.random() * num + 1);
@@ -64,4 +85,5 @@ function getFoodCord(num) {
 
 document.body.addEventListener("keydown", () =>{
     napravlrenie = event.keyCode;
+    console.log(napravlrenie);
 });
